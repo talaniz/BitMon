b2530c8a63ea05c320bd190bd1fdbae07e0c2218
Donald Brown <me@browntech.space>
2016-09-29 12:30:31 -0700

    Functionified the client file

--- client.py
+++ client.py
@@ -8,87 +8,67 @@ print("Checking for updates to the client...")
 webver = get(url="https://bitmonga.me/client").text
 with open(__file__,'r') as f: curver = f.read()
 if curver != webver:
-    print("New update found! Installing!")
-    with open(__file__,'w') as f: f.write(webver)
-    print("Installed! Relaunching...")
-    from subprocess import call
-    call(['python3',__file__])
-    exit()
+ print("New update found! Installing!")
+ with open(__file__,'w') as f: f.write(webver)
+ print("Installed! Relaunching...")
+ from subprocess import call
+ call(['python3',__file__])
+ exit()
 else: print("No update found!")
 
-
-srv = "https://bitmonga.me/"
-print("Welcome to the BitMon Client!")
-answer = int(input("Would you like to login[1] or register[2]?: "))
 ipaddr = get('https://api.ipify.org').text
+global logged_in
+logged_in = 0
+srv = 'https://bitmonga.me/'
 
-if answer == 1:
-    user = input("User name: ").lower()
-    password = getpass("Password: ").encode()
-    password = hashlib.md5(password).hexdigest()
-    srv += 'login?user={0}&pass={1}&ipaddr={2}'.format(user,password,ipaddr)
-    response = get(url=srv)
-    print(response.text)
 
-if answer == 2:
-    user = input("User name: ").lower()
-    if not user.isalnum() or not 6 <= len(user) <= 12:
-        print("Please enter only numbers and letters, between 6 and 12 characters.")
-        exit()
-    password = getpass("Password: ")
-    if not password.isalnum() or not 8<= len(password) <= 40:
-        print("Please enter only numbers and letters, between 8 and 40 characters.")
-        exit()
-    confirm = getpass("Re-type Password: ")
-    if not confirm == password:
-        print("Passwords do not match!")
-        exit()
-    password = password.encode()
-    password = hashlib.md5(password).hexdigest()
-    email = input("Email address: ")
-    srv += 'register?user={0}&pass={1}&ipaddr={2}&email={3}'.format(user,password,ipaddr,email)
-    response = get(url=srv)
-    print(response.text)
-    starter_select = int(input('''
-========================================================
------------------Choose a starter-----------------------
-========================================================
-Stats    |  [1]Yursba   |  [2]Grumsden |  [3]Hermyle
---------------------------------------------------------
-Type     | Nova         | Wind         | Earth
-Attack   | 17           | 15           | 20
-Defense  | 12           | 13           | 5
-========================================================
-Your choice: 
-'''))
-    if starter_select == 1:
-        srv = 'https://bitmonga.me/selectstarter?user={0}&pass={1}&ipaddr={2}&starter=Yursba'.format(user,password,ipaddr)
-        response = get(url=srv)
-        print(response.text)
-    if starter_select == 2:
-        srv = 'https://bitmonga.me/selectstarter?user={0}&pass={1}&ipaddr={2}&starter=Grumsden'.format(user,password,ipaddr)
-        response = get(url=srv)
-        print(response.text)
-    if starter_select == 3:
-        srv = 'https://bitmonga.me/selectstarter?user={0}&pass={1}&ipaddr={2}&starter=Hermyle'.format(user,password,ipaddr)
-        response = get(url=srv)
-        print(response.text)
+def main_menu():
+ global logged_in
+ if logged_in == 0:
+  answer = int(input('''======================
+| Welcome to Bitmon! |
+======================
+| 1 - Login          |
+----------------------
+| 2 - Register       |
+----------------------
+| 3 - Exit           |
+======================
+Please make a selection: '''))
+  if answer == 1: login()
+  if answer == 2: register()
+  if answer == 3: exit()
+ else: exit()
 
-    exit()
+
+def login():
+ global logged_in
+ user = input("Username: ").lower()
+ password = getpass("Password: ").encode()
+ password = hashlib.md5(password).hexdigest()
+ response = get(srv + 'login?user={0}&pass={1}&ipaddr={2}'.format(user,password,ipaddr)).text
+ if "correct" in response:
+  print(response)
+ else:
+  logged_in = 1
+  print(response)
+
+def register():
+ while True:
+  user = input("Username: ").lower()
+  if not 6 <= len(user) <= 12 or not user.isalnum():
+   print("Please choose an alphanumeric username between 6 and 12 characters.")
+   continue
+  password = getpass("Password: ")
+  confirm = getpass("Confirm Password: ")
+  if password != confirm:
+   print("Passwords do not match!")
+   continue
+  password = hashlib.md5(password.encode()).hexdigest()
+  email = input("Email address: ")
+  break
+ print(get(srv + 'register?user={0}&pass={1}&ipaddr={2}&email={3}'.format(user,password,ipaddr,email)).text)
 
 
 while True:
-    srv = "https://bitmonga.me/"
-    print("What would you like to do?")
-    choice = int(input('''
-==============================
------------Main Menu----------
-==============================
-1 - Log out
-==============================
-
-Your choice: 
-'''))
-    if choice == 1:
-        print("Goodbye!")
-        exit()
+ main_menu()
